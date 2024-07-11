bc8289f6c5017d5af60876d9ab5c0501f8832baa
Kipperish <152880818+Kipperish@users.noreply.github.com>
2024-07-11 12:59:55 +0100

    Update Buttons.py placed buttons in button file

--- Buttons.py
+++ Buttons.py
@@ -1,8 +1,6 @@
 import pygame
 
 # Button Class Which Handles Player Clicks
-
-
 class Button:
     def __init__(self, text, position, width, height, colour, hoverColour):
         self.text = text
@@ -48,3 +46,47 @@ class LevelButton(Button):
         Button.__init__(self, text, position, width,
                         height, colour, hoverColour)
         self.level = level
+
+
+# Colour RGB Values
+white = (255, 255, 255)
+orange = (255, 169, 10)
+lightOrange = (220, 185, 120)
+grey = (153, 153, 153)
+darkGrey = (120, 120, 120)
+black = (38, 38, 38)
+
+pygame.font.init()
+
+# Creates the different Buttons used in the program
+playButton = Button("Play", (540-100, 360), 200, 75, lightOrange, orange)
+
+settingsButton = Button("Settings", (440, 450), 200, 75, grey, darkGrey)
+
+homeButton = Button("Home", (20, 10), 90, 50, grey, darkGrey)
+
+quitButton = Button("Quit", (20, 10), 90, 50, grey, darkGrey)
+
+changeColourButton = Button(
+    "Change Colour", (440, 600), 200, 75, grey, darkGrey)
+
+changeVehicleButton = Button(
+    "Change Vehicle", (440, 500), 200, 75, grey, darkGrey)
+
+level1Button = LevelButton("level 1", (75, 100),
+                           275, 100, lightOrange, orange, None)
+
+level2Button = LevelButton("level 2", (400, 100),
+                           275, 100, lightOrange, orange, None)
+
+level3Button = LevelButton("level 3", (725, 100),
+                           275, 100, lightOrange, orange, None)
+
+level4Button = LevelButton("level 4", (75, 300),
+                           275, 100, lightOrange, orange, None)
+
+level5Button = LevelButton("level 5", (400, 300),
+                           275, 100, lightOrange, orange, None)
+
+level6Button = LevelButton("level 6", (725, 300),
+                           275, 100, lightOrange, orange, None)
