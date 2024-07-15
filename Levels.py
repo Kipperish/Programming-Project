91b95d27412ec5fb7c57a940a1d3f371e11c74b4
Kipperish <152880818+Kipperish@users.noreply.github.com>
2024-07-15 15:20:29 +0100

    Create Levels.py

---
+++ Levels.py
@@ -0,0 +1,9 @@
+import pygame
+from Colours import *
+
+
+class Level1Roundabout():
+    def __init__(self, player, screen):
+        self.player = player
+        self.screen = screen
+        self.backgroundColour = green
