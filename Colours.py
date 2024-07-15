2cd1b2ea9ca63c3e7a687aec6018b02696667312
Kipperish <152880818+Kipperish@users.noreply.github.com>
2024-07-15 15:19:39 +0100

    Create Colours.py

---
+++ Colours.py
@@ -0,0 +1,8 @@
+# Colour RGB Values
+white = (255, 255, 255)
+orange = (255, 169, 10)
+lightOrange = (220, 185, 120)
+grey = (153, 153, 153)
+darkGrey = (120, 120, 120)
+black = (38, 38, 38)
+green = (124, 252, 0)
