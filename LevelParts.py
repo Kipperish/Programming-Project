87fbcb045c1ddebf50ab0e780541d9117310edd1
Kipperish <152880818+Kipperish@users.noreply.github.com>
2024-08-06 10:16:27 +0100

    Update LevelParts.py

--- LevelParts.py
+++ LevelParts.py
@@ -1,8 +1,17 @@
 import pygame
 from Colours import *
 
-
+#Road class to implement road features into levels
 class Road():
-    def __init__(self, width, height, centrePos):
+    def __init__(self, width, length, centrePos):
         self.width = width
-        self.height = height
+        self.length = length
+        self.centrePos = centrePos
+        self.rect = pygame.Rect(0, 0, self.length, self.width)
+        self.rect.center = self.centrePos
+        self.colour = grey
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.colour, self.rect)
+
+straightRoad = Road(150, 1080, (540, 360))
